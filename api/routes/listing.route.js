4184cc99270be66c6b84f938527faf91383bbd8e
GideonAg <gideonagbosu72@gmail.com>
2024-02-29 09:48:18 +0000

    Added create listing api route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import { createListing } from "../controllers/listing.controller.js";
+
+const listingRouter = Router();
+
+listingRouter.post("/create", verifyToken, createListing);
+
+export default listingRouter;
